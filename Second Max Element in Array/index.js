da7435943a5f330d236a01f408ecacf994006909
Abishek <130296736+AbishekCodeCraft@users.noreply.github.com>
2025-01-17 09:46:28 +0530

    updated commit

--- Second Max Element in Array/index.js
+++ Second Max Element in Array/index.js
@@ -1,3 +1,5 @@
+
+// 1st Method
 function solution(arr) {
   if (arr.length <= 1) {
     return "No second max elemnt in an array";
@@ -22,8 +24,23 @@ function solution(arr) {
   return secondMax;
 }
 
+//2nd Method
+function answer(arr) {
+  if (arr.length <= 1) {
+    return null;
+  }
+
+  let max = arr.sort((a, b) => a - b); //Ascending Order
+  return max[max.length - 2];
+}
+
 var arr = [5, 8, 1, 4, 2, 9, 0, 3, 6, 7];
 console.log(solution(arr));
 console.log(solution([2]));
 console.log(solution([]));
 console.log(solution([2, 2]));
+
+
+console.log(answer(arr));
+console.log(answer([]));
+console.log(answer([5,2]));
