ed4eddab808d4d734debebd8fe6cb1799e495027
Abishek <130296736+AbishekCodeCraft@users.noreply.github.com>
2025-01-25 10:07:58 +0530

    Add files via upload

---
+++ Descending Order/index.js
@@ -0,0 +1,36 @@
+function solution(arr) {
+  if (arr.length == 0) {
+    return null;
+  }
+
+  if (arr.length == 1) {
+    return arr;
+  }
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+function answer(arr) {
+  if (arr.length == 0) {
+    return null;
+  }
+
+  if (arr.length == 1) {
+    return arr;
+  }
+
+  return arr.sort((a, b) => b - a);
+}
+
+var arr = [1, 5, 3, 6, 7, 2, 8, 9, 4, 0];
+
+console.log(solution(arr));
+console.log(answer(arr));
